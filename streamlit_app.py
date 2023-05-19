68cb3256b50bcee06bf69449fe992bbd16a433ae
tcor0005 <tcor0005@student.monash.edu>
2023-05-19 18:02:56 +1000

    Added my changes to the streamlit_app.py file and added the MICE.Rmd file for doing the MICE imputation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ def chatbot_page():
                         st.session_state.message_history.append({"role": "assistant", "content": completion_text})
                 
         # This is just for debugging, comment out before we deploy.
-        st.write(st.session_state.message_history)
+        # st.write(st.session_state.message_history)
 
 # Header creation & page configuration
 st.set_page_config(page_title="FIT3164 The Australian Marine Ecosystem Monitor", page_icon=":ocean:", layout="wide")
@@ -125,11 +125,11 @@ os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 if selected == "Home":
     tableau_public_embed_code = """
     <div class='tableauPlaceholder' id='viz1684482528778' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;FI&#47;FIT3164_16844217808070&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='FIT3164_16844217808070&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;FI&#47;FIT3164_16844217808070&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1684482528778');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.minWidth='100px';vizElement.style.maxWidth='1809px';vizElement.style.width='100%';vizElement.style.minHeight='823px';vizElement.style.maxHeight='1023px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.minWidth='100px';vizElement.style.maxWidth='1809px';vizElement.style.width='100%';vizElement.style.minHeight='823px';vizElement.style.maxHeight='1023px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='1227px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
-    st.components.v1.html(tableau_public_embed_code, width = 1000, height = 1000)
+    st.components.v1.html(tableau_public_embed_code, width = 1550, height = 1000)
 
 
-    df = pd.read_csv("Inner.csv")
-    st.map(df)
+    # df = pd.read_csv("Inner.csv")
+    # st.map(df)
 
 # Chatbot Page
 elif selected == "Chatbot":
